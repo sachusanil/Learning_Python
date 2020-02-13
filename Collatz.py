09f357d3deb678419251a7a6b4b3c2a7d962edc5
sachusanil <56233123+sachusanil@users.noreply.github.com>
2020-02-13 17:11:59 +0100

    Functional Prgramme

---
+++ Collatz.py
@@ -0,0 +1,28 @@
+'''Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and return this value. If number is odd, then collatz() should print and return 3 * number + 1.
+
+Then write a program that lets the user type in an integer and that keeps calling collatz() on that number until the function returns the value 1. (Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why. Your program is exploring what’s called the Collatz sequence, sometimes called “the simplest impossible math problem.”)'''
+
+
+def collatz(number):
+    
+    if number % 2 == 0:
+        print(number//2)
+        return (number//2)
+    else:
+        print (3*number+1)
+        return (3*number+1)
+
+print("Enter Number:", end=' ')
+num = input()
+
+
+try:
+	while True:
+		if (int(num)!=1):
+			num=collatz(int(num))
+		else:
+			print('End of Loop')
+			break
+except ValueError:
+	print('Enter a number, not a string')
+
